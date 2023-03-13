38cc70ce4570868594a31ed9e810e52da393f6f2
alvachon <alvachon93@pm.me>
2023-03-13 15:15:23 -0400

    prep to push

--- philo/srcs/action.c
+++ philo/srcs/action.c
@@ -30,19 +30,29 @@ void    time_to_sleep(t_thread *philo)
     waitsys(philo->info->time_to_sleep);
 }
 
-void    time_to_eat(t_thread *philo)
+void    lock_forks(t_thread *philo)
 {
     pthread_mutex_lock(philo->l_fork);
+    printeur(get_time() - philo->info->start, philo->thread_id, "as taken a fork", philo);
     pthread_mutex_lock(philo->r_fork);
     printeur(get_time() - philo->info->start, philo->thread_id, "as taken a fork", philo);
-    printeur(get_time() - philo->info->start, philo->thread_id, "as taken a fork", philo);
+}
+
+void    unlock_forks(t_thread *philo)
+{
+    pthread_mutex_unlock(philo->l_fork);
+    printeur(get_time() - philo->info->start, philo->thread_id, "as left a fork", philo);
+    pthread_mutex_unlock(philo->r_fork);
+    printeur(get_time() - philo->info->start, philo->thread_id, "as left a fork", philo);
+}
+
+void    time_to_eat(t_thread *philo)
+{
+    lock_forks(philo);
     printeur(get_time() - philo->info->start, philo->thread_id, "is eating", philo);
     waitsys(philo->info->time_to_eat);
     (philo)->last_meal = get_time();
     if (philo->nb_meal != -1)
         (philo)->nb_meal += 1;
-    pthread_mutex_unlock(philo->l_fork);
-    pthread_mutex_unlock(philo->r_fork);
-    printeur(get_time() - philo->info->start, philo->thread_id, "as left a fork", philo);
-    printeur(get_time() - philo->info->start, philo->thread_id, "as left a fork", philo);
+    unlock_forks(philo);
 }
